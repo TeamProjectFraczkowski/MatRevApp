2bc8d813bbf2c9eb924a741af9ea6362fb43daba
binaspaulina1@gmail.com <kitka988>
2017-05-14 10:59:15 +0200

    Added Revision Slide for topic 1

--- app/src/main/java/com/example/rupal/matrev/RevisionActivity.java
+++ app/src/main/java/com/example/rupal/matrev/RevisionActivity.java
@@ -1,7 +1,9 @@
 package com.example.rupal.matrev;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by rupal on 23/04/2017.
@@ -13,5 +15,10 @@ public class RevisionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_revision);
+    }
+
+    public void onTopic1ButtonClick(View view){
+        Intent topic1Activity = new Intent(this, RevisionTopicActivity.class);
+        startActivity(topic1Activity);
     }
 }
