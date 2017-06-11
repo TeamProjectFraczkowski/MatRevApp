ceab5375fcf289aa1df02ed99939052d99a255a0
SharmaRupali <rups1501@gmail.com>
2017-06-11 10:32:02 +0200

    Login Orientation

--- app/src/main/java/com/example/rupal/matrev/LoginActivity.java
+++ app/src/main/java/com/example/rupal/matrev/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.rupal.matrev;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -80,5 +82,11 @@ public class LoginActivity extends AppCompatActivity
         loginActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(loginActivity);
         finish();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newCOnfig){
+        super.onConfigurationChanged(newCOnfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
